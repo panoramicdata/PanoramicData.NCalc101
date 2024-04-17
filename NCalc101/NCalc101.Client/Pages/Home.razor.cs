61442827791d855000026d863e16332ddb84ec94
David Bond <david.bond@panoramicdata.com>
2024-04-17 14:44:10 +0100

    Updated nugets

--- NCalc101/NCalc101.Client/Pages/Home.razor.cs
+++ NCalc101/NCalc101.Client/Pages/Home.razor.cs
@@ -1,13 +1,6 @@
-using PanoramicData.ReportMagic.Data;
-
 namespace NCalc101.Client.Pages;
 
 public partial class Home
 {
-	private Person? _person;
-
 	[Inject] private IMagicSuiteService MagicSuiteService { get; set; } = null!;
-
-	protected override async Task OnInitializedAsync()
-		=> _person = await MagicSuiteService.GetUserInfoAsync(default);
 }
